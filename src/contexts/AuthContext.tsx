0b9da38b73993d5214d7a6083178144f385d75ce
João Prestes <jvictor1949@hotmail.com>
2021-06-23 01:54:50 -0300

    Finalizado aula 2

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,68 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { firebase,auth } from "../services/firebase";
+
+type User = {
+    id: string;
+    name: string;
+    avatar: string;
+  }
+  
+  type AuthContextType = {
+    user: User | undefined;
+    singInWithGoogle: () => Promise<void>;
+  
+  }
+  type AuthContextProviderProps = {
+      children: ReactNode;
+  }
+  
+export const AuthContext = createContext({} as AuthContextType);
+
+export function AuthContextProvider(props: AuthContextProviderProps ){
+    const [user, setUser] = useState<User>();
+    useEffect(() => {
+        auth.onAuthStateChanged(user => {
+          if (user) {
+            const { displayName, photoURL, uid } = user
+    
+            if (!displayName || !photoURL) {
+              throw new Error('Deu erro viado');
+            }
+    
+            setUser({
+              id: uid,
+              name: displayName,
+              avatar: photoURL
+            })
+    
+          }
+        })
+      }, [])
+    
+      async function singInWithGoogle() {
+        const provider = new firebase.auth.GoogleAuthProvider();
+    
+        const result = await auth.signInWithPopup(provider);
+    
+        console.log(result);
+        if (result.user) {
+          const { displayName, photoURL, uid } = result.user
+    
+          if (!displayName || !photoURL) {
+            throw new Error('Deu erro viado');
+          }
+    
+          setUser({
+            id: uid,
+            name: displayName,
+            avatar: photoURL
+          })
+        }
+    
+      }
+    return(
+        <AuthContext.Provider value={{ user, singInWithGoogle }}>
+            {props.children}
+        </AuthContext.Provider>
+    )
+}
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,12 +1,17 @@
-import illustrationImg from '../assets/images/illustration.svg'
+
+import {Link} from 'react-router-dom'
 import {Button} from '../components/Button'
+import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 
 import '../styles/auth.scss';
+import { useAuth } from '../hooks/useAuth';
 
 
 export function NewRoom() {
-    return (
+    const {user} = useAuth();
+
+       return (
         <div id="page-auth">
             <aside>
                 <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
@@ -14,6 +19,7 @@ export function NewRoom() {
                 <p>Tire suas dúvidas em tempo-real</p>
             </aside>
             <main>
+                
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
                     <h2>Criar uma nova sala</h2>                   
@@ -27,7 +33,7 @@ export function NewRoom() {
                         </Button>
                     </form>
                     <p>
-                        Quer entrar em um sala existente? <a href="#">clique aqui</a>
+                        Quer entrar em um sala existente? <Link to="/">clique aqui</Link>
                     </p>
                 </div>
             </main>
